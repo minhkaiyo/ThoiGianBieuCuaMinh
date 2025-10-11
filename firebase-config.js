97b768a78b31fd8be043deffc0125a4c2fd1c1c6
minhkaiyo <155993106+minhkaiyo@users.noreply.github.com>
2025-10-11 21:27:30 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,17 @@
 // firebase-config.js
 
+// Import các hàm bạn cần từ CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
 
+// Cấu hình Firebase của ứng dụng web
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
+    apiKey: "AIzaSyBjmeNXGaqpXTmC6dVkiOfKKJ_WJPoVB-g",
     authDomain: "thoi-gian-bieu-cua-minh.firebaseapp.com",
     projectId: "thoi-gian-bieu-cua-minh",
-    storageBucket: "thoi-gian-bieu-cua-minh.appspot.com",
+    storageBucket: "thoi-gian-bieu-cua-minh.firebasestorage.app",
     messagingSenderId: "685544265161",
     appId: "1:685544265161:web:ba2fdcfb1dbd2311238418",
     measurementId: "G-ZKPS6WNLXK"
@@ -16,7 +19,8 @@ const firebaseConfig = {
 
 // Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app); // Khởi tạo Analytics
 
-// Khởi tạo và EXPORT các dịch vụ để file khác có thể dùng
+// Khởi tạo và EXPORT các dịch vụ để các tệp khác có thể dùng
 export const auth = getAuth(app);
 export const db = getFirestore(app);
